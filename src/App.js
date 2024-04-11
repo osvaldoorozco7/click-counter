cabc962c5a8344c012507f99f64dcb7ddad91b22
Osvaldo <osvaldoorozcorod@gmail.com>
2024-04-10 20:21:20 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import codemonkeyLogo from './Imagenes/the-monkey-svgrepo-com.svg';
+import Boton from './componentes/Boton.jsx';
+import Contador from './componentes/Contador.jsx';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='codemonkey-logo-contenedor'>
+        <img 
+        className='codemonkey-logo'
+        src={codemonkeyLogo}
+        alt='Logo de Code Monkey' />
+      </div>
+
+      <div className='contenedor-principal'>
+
+        <Contador numClicks={numClicks} />
+
+        <Boton
+        texto= 'Click'
+        esBotonDeClick={true}
+        manejarClick={manejarClick} />
+
+        <Boton
+        texto= 'Reiniciar'
+        esBotonDeClick={false}
+        manejarClick={reiniciarContador} />
+
+      </div>
+    
     </div>
   );
 }
